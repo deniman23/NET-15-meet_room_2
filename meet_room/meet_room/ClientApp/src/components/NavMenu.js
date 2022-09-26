365abe5f21677de004c1c5d40a933ab951cb4b3d
Daniil Tolkachev <itdanetollk@gmail.com>
2022-09-26 19:01:19 +0300

    comit

--- meet_room/meet_room/ClientApp/src/components/NavMenu.js
+++ meet_room/meet_room/ClientApp/src/components/NavMenu.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
-import "https://fonts.googleapis.com/css2?family=Raleway:wght@600&display=swap";
-import "https://fonts.googleapis.com/css2?family=Krona+One&display=swap";
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -28,7 +26,7 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="block_raw" container light>
-          <NavbarBrand tag={Link} to="/" className="rooms_logo"><img src="../wwwroot/assets/images/logo.svg" width="60px" height="60px" alt="logo"></img></NavbarBrand>
+          <NavbarBrand tag={Link} to="/" className="rooms_logo"><img src={require('./images/logo.svg')} style={nbStyle.logo} alt="fireSpot"/></NavbarBrand>
           <NavbarBrand tag={Link} to="/" className="rooms_text1">Rooms</NavbarBrand>
           <NavbarBrand tag={Link} to="/" className="rooms_enter">UserName</NavbarBrand>
         </Navbar>
